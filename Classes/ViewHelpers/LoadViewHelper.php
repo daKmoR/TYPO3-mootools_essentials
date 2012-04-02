e6e44cfc67b88a97e1586e9918dddbc52a80f0df
daKmoR <thomas.allmer@webteam.at>
2012-04-03 01:58:15 +0200

    cosmetical changes

--- Classes/ViewHelpers/LoadViewHelper.php
+++ Classes/ViewHelpers/LoadViewHelper.php
@@ -32,7 +32,6 @@
  * </output>
  *
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
- * @api
  */
 class Tx_MootoolsEssentials_ViewHelpers_LoadViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
 
